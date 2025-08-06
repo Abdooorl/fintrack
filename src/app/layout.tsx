25dc24cb79e1a39605705cc9714954dce40e430b
Abdooorl <badamosiabdullahi@gmail.com>
2025-08-06 18:57:39 +0100

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Public_Sans } from "next/font/google";
+import "./globals.css";
+
+const publicSans = Public_Sans({
+  variable: "--font-public-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Fintrack",
+  description: "fintrack FInances",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${publicSans.variable}  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <Link href="/home/dashboard"> Go to Dashboard </Link>
+    </div>
+  );
+}
